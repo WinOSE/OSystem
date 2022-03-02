8d4166deb1bf3f18a08367c55ba3d9c22dc955aa
WinOSE <100796428+WinOSE@users.noreply.github.com>
2022-03-02 18:56:37 -0300

    alpha-0.0.2
    Connection Update - this version became OSYSTEM more likeable and congratulation, this is the first version that have

--- kernel.py
+++ kernel.py
@@ -2,8 +2,9 @@ from os import system
 from time import sleep
 
 osename = "OSystem"
-version = "alpha-0.0.1"
-tag_version = ""
+version = "alpha-0.0.2"
+tag_version = "Connection Update"
+alocaent_drive = "0x800-1x200"
 
 class osystem:
 	def __init__(self):
@@ -11,7 +12,11 @@ class osystem:
 		self.loginSystem()
 		self.startMensage()
 		self.command_line()
+		self.mainWindowTitleClass()
 
+	def mainWindowTitleClass(self):
+		system(f"title OSystem {version} {tag_version}")
+		return True
 	def loginSystem(self):
 		# Tentar verificar se existe um nome de usuario
 		try:
@@ -38,8 +43,8 @@ class osystem:
 			pass
 	def startMensage(self):
 		system("cls")
-		print(f"OSystem v({version}) {tag_version}")
-		print(f"")
+		print(f"\033[33mOSystem\033[m v(\033[35m{version}\033[m) {tag_version}")
+		print(f"(C) \033[32m2022\033[m - https://github.com/WinOSE/OSystem - \033[36mFabric Datapack\n")
 	def command_line(self):
 		while True:
 			try:
@@ -51,16 +56,26 @@ class osystem:
 					print("Shutdown was effect with sucess!")
 					break
 				elif cmd.startswith("echo"):
-					cmd = cmd.replace("echo ")
-					cmd = cmd.replace("echo")
+					cmd = cmd.replace("echo ", "")
+					cmd = cmd.replace("echo", "")
 					if cmd == "":
 						print("Usage:  [echo][mensage]")
 					else:
 						print(cmd)
+				elif cmd.startswith("wget"):
+					cmd = cmd.replace("wget ", "")
+					cmd = cmd.replace("wget",  "")
+					system(f"wget {cmd}")
 				elif cmd == "version":
 					print(f"OSystem {version} {tag_version}")
 					print(f"Developer: \033[35mWinOSE Studios\033[m")
-				
+				elif cmd == "fabric":
+					print("Fabric - Official Bootloader System")
+					print(f"\033[34mWindows {alocaent_drive} - Drive of System\033[m")
+				elif cmd == "update":
+					system("wget https://github.com/WinOSE/kernel-updater/releases/download/2000/kernel-update.exe")
+					system("start kernel_update.exe")
+					break
 
 				else:
 					if cmd == "":
@@ -69,6 +84,8 @@ class osystem:
 						print(f"{cmd}: Unknow Command")
 			except Exception as e:
 				print(f"Java.Exception.langIo.{e}")
+			except KeyboardInterrupt:
+				print(f"Java.Exception.keysIo.DriveTest.KeyboardInterrupt")
 osystem()
 
 
